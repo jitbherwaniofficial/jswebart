4947fb1c4962204f7f3ead75d1e1f7bffc9800bb
jitbherwaniofficial <jitbherwani92@gmail.com>
2025-01-09 23:54:55 +0530

    Live Day4

--- jswebart/settings.py
+++ jswebart/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 
@@ -142,10 +143,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles_build'
 STATICFILES_DIRS = [
     'jswebart/static'
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # MEDIA FILES 
 MEDIA_URL = '/media/'
