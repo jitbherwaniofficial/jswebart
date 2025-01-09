196b77a564f8dafe816d856e62c1c885d1f24828
jitbherwaniofficial <jitbherwani92@gmail.com>
2025-01-10 00:16:47 +0530

    Live Day9

--- build_files.sh
+++ build_files.sh
@@ -1,19 +1,8 @@
-echo "BUILD START"
-
-# Use python3 to ensure correct installation
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-
-# Collect static files
-python3 manage.py collectstatic --noinput --clear
-
-echo "BUILD END"
 
 
 
-
-# echo " BUILD START"
-# pip install -r requirements.txt
-# python3.9 manage.py collectstatic --noinput --clear
-# echo " BUILD END" 
+echo " BUILD START"
+pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+echo " BUILD END" 
 
