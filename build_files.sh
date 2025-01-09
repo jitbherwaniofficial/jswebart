73c45de87b90e6707435b54f6953c4f9eb0dbaf6
jitbherwaniofficial <jitbherwani92@gmail.com>
2025-01-10 02:36:54 +0530

    Live13

--- build_files.sh
+++ build_files.sh
@@ -5,13 +5,30 @@
 
 #!/bin/bash
 
+# echo "BUILD START"
+
+# # Upgrade pip and install dependencies
+# python3 -m pip install --upgrade pip
+# python3 -m pip install -r requirements.txt
+
+# # Collect static files
+# python3 manage.py collectstatic --noinput --clear
+
+# echo "BUILD END"
+
+
 echo "BUILD START"
 
+# Create and activate a virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
 # Upgrade pip and install dependencies
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
 
 # Collect static files
 python3 manage.py collectstatic --noinput --clear
+
 
 echo "BUILD END"
