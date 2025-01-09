2755fa6ddfd95f2e2031ada78e378f0d8ef21549
jitbherwaniofficial <jitbherwani92@gmail.com>
2025-01-10 01:57:22 +0530

    Live10

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,12 @@
+# echo " BUILD START"
+# pip install -r requirements.txt
+# python3.9 manage.py collectstatic --noinput --clear
+# echo " BUILD END"
 
-
-
+#!/bin/bash
 echo " BUILD START"
-python3 -m ensurepip --upgrade
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+python3 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo " BUILD END" 
-
+echo " BUILD END"
