3cffa18a945d4aefb39e0c0d06f2b2f077b548d7
jitbherwaniofficial <jitbherwani92@gmail.com>
2025-03-11 01:35:08 +0530

    Deleted Recaptcha

--- jswebart/context_processors.py
+++ jswebart/context_processors.py
@@ -1,7 +1,6 @@
 from blog.forms import SubscriptionForm
-from decouple import config
 
 def subscription_form(request):
     form = SubscriptionForm()
-    SHARE_SITE_KEY = config('SHARE_SITE_KEY')
-    return {'subscription_form': form, 'SHARE_SITE_KEY':SHARE_SITE_KEY}
+    
+    return {'subscription_form': form}
