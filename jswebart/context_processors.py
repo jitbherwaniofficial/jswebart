8814590ff5a0087f23da9b9ef2b4136d605cf06f
jitbherwaniofficial <jitbherwani92@gmail.com>
2025-03-11 01:27:10 +0530

    Recaptcha on subscribe form

--- jswebart/context_processors.py
+++ jswebart/context_processors.py
@@ -1,5 +1,7 @@
 from blog.forms import SubscriptionForm
+from decouple import config
 
 def subscription_form(request):
     form = SubscriptionForm()
-    return {'subscription_form': form}
+    SHARE_SITE_KEY = config('SHARE_SITE_KEY')
+    return {'subscription_form': form, 'SHARE_SITE_KEY':SHARE_SITE_KEY}
